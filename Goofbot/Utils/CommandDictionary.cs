72f13b59dce99804d70a59622947475bfe9e492c
Jack Tackett <jeanvaljean563@gmail.com>
2025-08-20 08:53:04 -0500

    StyleCop CommandDictionary.cs

--- Goofbot/Utils/CommandDictionary.cs
+++ Goofbot/Utils/CommandDictionary.cs
@@ -1,28 +1,23 @@
-﻿using System;
+﻿namespace Goofbot.Utils;
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Goofbot.Utils
+internal class CommandDictionary
 {
-    internal class CommandDictionary
+    private readonly Dictionary<string, Command> commandDictionary = new ();
+
+    public CommandDictionary()
     {
-        private readonly Dictionary<string, Command> _commandDictionary = new();
 
-        public CommandDictionary()
-        {
+    }
 
-        }
+    public bool TryAddCommand(Command command)
+    {
+        return this.commandDictionary.TryAdd(command.Name, command);
+    }
 
-        public bool TryAddCommand(Command command)
-        {
-            return _commandDictionary.TryAdd(command.Name, command);
-        }
-
-        public bool TryGetCommand(string name, out Command command)
-        {
-            return _commandDictionary.TryGetValue(name, out command);
-        }
+    public bool TryGetCommand(string name, out Command command)
+    {
+        return this.commandDictionary.TryGetValue(name, out command);
     }
 }
