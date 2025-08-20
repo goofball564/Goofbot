02c92e5c2601c1ae5bdc58625c5383b2ed024cb1
Jack Tackett <jeanvaljean563@gmail.com>
2025-08-19 22:40:35 -0500

    removed unnecessary using statements

--- Goofbot/Modules/SpotifyModule.cs
+++ Goofbot/Modules/SpotifyModule.cs
@@ -5,8 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using TwitchLib.Api;
-using TwitchLib.Client;
 using TwitchLib.Client.Events;
 
 namespace Goofbot.Modules
