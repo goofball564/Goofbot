aed6b991e13c5ed240f167216f6d67dc08b62c42
Jack Tackett <jeanvaljean563@gmail.com>
2025-08-20 09:03:03 -0500

    StyleCop TwitchAuthenticationManager.cs

--- Goofbot/Utils/TwitchAuthenticationManager.cs
+++ Goofbot/Utils/TwitchAuthenticationManager.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿namespace Goofbot.Utils;
+
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,148 +12,145 @@ using TwitchLib.Api;
 using TwitchLib.Client;
 using TwitchLib.Client.Models;
 
-namespace Goofbot.Utils
+internal class TwitchAuthenticationManager
 {
-    internal class TwitchAuthenticationManager
+    public const string TwitchAppRedirectUrl = "http://localhost:3000/";
+    public const string TwitchAuthorizationCodeRequestUrlBase = "https://id.twitch.tv/oauth2/authorize";
+    public const string TwitchTokenRequestUrl = "https://id.twitch.tv/oauth2/token";
+
+    private static readonly List<string> BotScopes = ["user:read:chat", "user:write:chat", "user:bot", "chat:read", "chat:edit"];
+    private static readonly List<string> ChannelScopes = ["channel:bot", "channel:read:redemptions"];
+
+    private readonly HttpClient httpClient = new ();
+    private readonly WebServer server = new (TwitchAppRedirectUrl);
+
+    private readonly SemaphoreSlim webServerSemaphore = new (1, 1);
+    private readonly SemaphoreSlim botTokensSemaphore = new (1, 1);
+    private readonly SemaphoreSlim channelTokensSemaphore = new (1, 1);
+
+    private string twitchClientID;
+    private string twitchClientSecret;
+    private string twitchBotAccessToken;
+    private string twitchChannelAccessToken;
+
+    private TwitchClient twitchClient;
+    private TwitchAPI twitchAPI;
+
+    public TwitchAuthenticationManager(string twitchClientID, string twitchClientSecret, TwitchClient twitchClient, TwitchAPI twitchAPI)
     {
-        public const string TwitchAppRedirectUrl = "http://localhost:3000/";
-        public const string TwitchAuthorizationCodeRequestUrlBase = "https://id.twitch.tv/oauth2/authorize";
-        public const string TwitchTokenRequestUrl = "https://id.twitch.tv/oauth2/token";
+        this.twitchClientID = twitchClientID;
+        this.twitchClientSecret = twitchClientSecret;
+        this.twitchClient = twitchClient;
+        this.twitchAPI = twitchAPI;
+    }
 
-        private static readonly List<string> s_botScopes = ["user:read:chat", "user:write:chat", "user:bot", "chat:read", "chat:edit"];
-        private static readonly List<string> s_channelScopes = ["channel:bot", "channel:read:redemptions"];
+    public async Task Initialize()
+    {
+        Task botAccessTokenTask = this.RefreshTwitchAccessToken(true);
+        Task channelAccessTokenTask = this.RefreshTwitchAccessToken(false);
 
-        private readonly HttpClient _httpClient = new();
-        private readonly WebServer _server = new(TwitchAppRedirectUrl);
+        await botAccessTokenTask;
+        await channelAccessTokenTask;
 
-        private readonly SemaphoreSlim _webServerSemaphore = new(1, 1);
-        private readonly SemaphoreSlim _botTokensSemaphore = new(1, 1);
-        private readonly SemaphoreSlim _channelTokensSemaphore = new(1, 1);
+        this.twitchAPI.Settings.ClientId = this.twitchClientID;
+        this.twitchAPI.Settings.AccessToken = this.twitchChannelAccessToken;
 
-        private string _twitchClientID;
-        private string _twitchClientSecret;
-        private string _twitchBotAccessToken;
-        private string _twitchChannelAccessToken;
+        var credentials = new ConnectionCredentials(Program.TwitchBotUsername, this.twitchBotAccessToken);
+        this.twitchClient.Initialize(credentials, Program.TwitchChannelUsername);
+    }
 
-        private TwitchClient _twitchClient;
-        private TwitchAPI _twitchAPI;
+    private async Task RefreshTwitchAccessToken(bool botToken)
+    {
+        string tokensFile = botToken ? "bot_tokens.json" : "channel_tokens.json";
+        tokensFile = Path.Join(Program.StuffFolder, tokensFile);
 
-        public TwitchAuthenticationManager(string twitchClientID, string twitchClientSecret, TwitchClient twitchClient, TwitchAPI twitchAPI)
+        string code;
+        await this.webServerSemaphore.WaitAsync();
+        try
         {
-            _twitchClientID = twitchClientID;
-            _twitchClientSecret = twitchClientSecret;
-            _twitchClient = twitchClient;
-            _twitchAPI = twitchAPI;
+            code = await this.RequestTwitchAuthorizationCode(botToken);
+        }
+        finally
+        {
+            this.webServerSemaphore.Release();
         }
 
-        public async Task Initialize()
+        SemaphoreSlim semaphore = botToken ? this.botTokensSemaphore : this.channelTokensSemaphore;
+        await semaphore.WaitAsync();
+        try
         {
-            Task botAccessTokenTask = RefreshTwitchAccessToken(true);
-            Task channelAccessTokenTask = RefreshTwitchAccessToken(false);
+            string tokensString = await this.RequestTwitchTokensWithAuthorizationCode(code);
 
-            await botAccessTokenTask;
-            await channelAccessTokenTask;
+            CancellationToken cancellationToken = new ();
+            Task writeAllTextTask = File.WriteAllTextAsync(tokensFile, tokensString, cancellationToken);
 
-            _twitchAPI.Settings.ClientId = _twitchClientID;
-            _twitchAPI.Settings.AccessToken = _twitchChannelAccessToken;
-
-            var credentials = new ConnectionCredentials(Program.TwitchBotUsername, _twitchBotAccessToken);
-            _twitchClient.Initialize(credentials, Program.TwitchChannelUsername);
-        }
-
-        private async Task RefreshTwitchAccessToken(bool botToken)
-        {
-            string tokensFile = botToken ? "bot_tokens.json" : "channel_tokens.json";
-            tokensFile = Path.Join(Program.StuffFolder, tokensFile);
-
-            string code;
-            await _webServerSemaphore.WaitAsync();
-            try
+            dynamic tokensObject = JsonConvert.DeserializeObject(tokensString);
+            if (botToken)
             {
-                code = await RequestTwitchAuthorizationCode(botToken);
-            }
-            finally
-            {
-                _webServerSemaphore.Release();
-            }
-
-            SemaphoreSlim semaphore = botToken ? _botTokensSemaphore : _channelTokensSemaphore;
-            await semaphore.WaitAsync();
-            try
-            {
-                string tokensString = await RequestTwitchTokensWithAuthorizationCode(code);
-
-                CancellationToken cancellationToken = new();
-                Task writeAllTextTask = File.WriteAllTextAsync(tokensFile, tokensString, cancellationToken);
-
-                dynamic tokensObject = JsonConvert.DeserializeObject(tokensString);
-                if (botToken)
-                {
-                    _twitchBotAccessToken = Convert.ToString(tokensObject.access_token);
-                }
-                else
-                {
-                    _twitchChannelAccessToken = Convert.ToString(tokensObject.access_token);
-                }
-
-                await writeAllTextTask;
-            }
-            finally
-            {
-                semaphore.Release();
-            }
-
-        }
-
-        private async Task<string> RequestTokensWithRefreshToken(string refreshToken)
-        {
-            var values = new Dictionary<string, string>
-            {
-                { "refresh_token", refreshToken },
-                { "client_id", Convert.ToString(_twitchClientID) },
-                { "client_secret", Convert.ToString(_twitchClientSecret) },
-                { "grant_type", "refresh_token" },
-            };
-            var content = new FormUrlEncodedContent(values);
-            var response = await _httpClient.PostAsync(TwitchTokenRequestUrl, content);
-            return await response.Content.ReadAsStringAsync();
-        }
-
-        private async Task<string> RequestTwitchTokensWithAuthorizationCode(string twitchAuthorizationCode)
-        {
-            var values = new Dictionary<string, string>
-            {
-                { "code", twitchAuthorizationCode },
-                { "client_id", Convert.ToString(_twitchClientID) },
-                { "client_secret", Convert.ToString(_twitchClientSecret) },
-                { "grant_type", "authorization_code" },
-                { "redirect_uri", TwitchAppRedirectUrl }
-            };
-            var content = new FormUrlEncodedContent(values);
-            var response = await _httpClient.PostAsync(TwitchTokenRequestUrl, content);
-            return await response.Content.ReadAsStringAsync();
-        }
-
-        private string GetTwitchAuthorizationCodeRequestUrl(List<string> scopes)
-        {
-            return $"{TwitchAuthorizationCodeRequestUrlBase}?" +
-                   $"client_id={_twitchClientID}&" +
-                   $"redirect_uri={System.Web.HttpUtility.UrlEncode(TwitchAppRedirectUrl)}&" +
-                   "response_type=code&" +
-                   $"scope={string.Join('+', scopes)}";
-        }
-
-        private async Task<string> RequestTwitchAuthorizationCode(bool useChrome)
-        {
-            if (useChrome)
-            {
-                Process.Start("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", GetTwitchAuthorizationCodeRequestUrl(s_botScopes));
+                this.twitchBotAccessToken = Convert.ToString(tokensObject.access_token);
             }
             else
             {
-                Process.Start(new ProcessStartInfo { FileName = GetTwitchAuthorizationCodeRequestUrl(s_channelScopes), UseShellExecute = true });
+                this.twitchChannelAccessToken = Convert.ToString(tokensObject.access_token);
             }
-            return await _server.Listen();
+
+            await writeAllTextTask;
         }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    private async Task<string> RequestTokensWithRefreshToken(string refreshToken)
+    {
+        var values = new Dictionary<string, string>
+        {
+            { "refresh_token", refreshToken },
+            { "client_id", Convert.ToString(this.twitchClientID) },
+            { "client_secret", Convert.ToString(this.twitchClientSecret) },
+            { "grant_type", "refresh_token" },
+        };
+        var content = new FormUrlEncodedContent(values);
+        var response = await this.httpClient.PostAsync(TwitchTokenRequestUrl, content);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    private async Task<string> RequestTwitchTokensWithAuthorizationCode(string twitchAuthorizationCode)
+    {
+        var values = new Dictionary<string, string>
+        {
+            { "code", twitchAuthorizationCode },
+            { "client_id", Convert.ToString(this.twitchClientID) },
+            { "client_secret", Convert.ToString(this.twitchClientSecret) },
+            { "grant_type", "authorization_code" },
+            { "redirect_uri", TwitchAppRedirectUrl },
+        };
+        var content = new FormUrlEncodedContent(values);
+        var response = await this.httpClient.PostAsync(TwitchTokenRequestUrl, content);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    private string GetTwitchAuthorizationCodeRequestUrl(List<string> scopes)
+    {
+        return $"{TwitchAuthorizationCodeRequestUrlBase}?" +
+               $"client_id={this.twitchClientID}&" +
+               $"redirect_uri={System.Web.HttpUtility.UrlEncode(TwitchAppRedirectUrl)}&" +
+               "response_type=code&" +
+               $"scope={string.Join('+', scopes)}";
+    }
+
+    private async Task<string> RequestTwitchAuthorizationCode(bool useChrome)
+    {
+        if (useChrome)
+        {
+            Process.Start("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", this.GetTwitchAuthorizationCodeRequestUrl(BotScopes));
+        }
+        else
+        {
+            Process.Start(new ProcessStartInfo { FileName = this.GetTwitchAuthorizationCodeRequestUrl(ChannelScopes), UseShellExecute = true });
+        }
+
+        return await this.server.Listen();
     }
 }
