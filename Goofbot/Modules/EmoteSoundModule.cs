cb8b1a9e60e64d69998a5ed1633d613c7996df2f
goofball564 <jeanvaljean563@gmail.com>
2025-08-21 20:11:09 -0500

    ugly code goes through every word in message, and plays sounds if it encounters specific words, in the order it finds the words

--- Goofbot/Modules/EmoteSoundModule.cs
+++ Goofbot/Modules/EmoteSoundModule.cs
@@ -3,31 +3,67 @@
 using Goofbot.Utils;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using TwitchLib.Client;
 using TwitchLib.Client.Events;
 
 internal partial class EmoteSoundModule : GoofbotModule
 {
-    private const string LizardFileName = "lizard.wav";
+    private const string EmoteListFileName = "emotes.txt";
 
-    private readonly string lizardFile;
+    private readonly string emoteListFile;
+
+    private readonly Dictionary<string, string> emoteSoundDictionary = [];
 
     public EmoteSoundModule(string moduleDataFolder, TwitchClient twitchClient)
         : base(moduleDataFolder)
     {
-        this.lizardFile = Path.Join(this.ModuleDataFolder, LizardFileName);
+        this.emoteListFile = Path.Join(this.ModuleDataFolder, EmoteListFileName);
+        this.ParseTheThing();
+
         twitchClient.OnMessageReceived += this.Client_OnMessageReceived;
     }
 
-    [GeneratedRegex("(?<=\\b)LIZARD(?=\\b)")]
-    private static partial Regex LizardRegex();
+    [GeneratedRegex("(?<=\\b)\\S+(?=\\b)")]
+    private static partial Regex WordRegex();
 
-    private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+    private void ParseTheThing()
     {
-        string message = e.ChatMessage.Message.Trim();
-        int lizardCount = LizardRegex().Count(message);
-        new SoundPlayer(this.lizardFile, numberTimesToLoop: lizardCount);
+        string soundFile = string.Empty;
+        foreach (string line in File.ReadLines(this.emoteListFile))
+        {
+            if (line.Equals(string.Empty))
+            {
+                soundFile = string.Empty;
+            }
+            else if (soundFile.Equals(string.Empty))
+            {
+                soundFile = line.Trim();
+            }
+            else
+            {
+                this.emoteSoundDictionary.TryAdd(line, Path.Join(this.ModuleDataFolder, soundFile));
+            }
+        }
+    }
+
+    private async void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+    {
+        string message = e.ChatMessage.Message;
+        Stopwatch stopwatch = new ();
+        stopwatch.Start();
+
+        foreach (Match match in WordRegex().Matches(message))
+        {
+            if (this.emoteSoundDictionary.TryGetValue(match.Value, out string soundFile))
+            {
+                await Task.Delay(500 - (int)stopwatch.ElapsedMilliseconds);
+                new SoundPlayer(soundFile, volume: 0.075f);
+                stopwatch.Restart();
+            }
+        }
     }
 }
