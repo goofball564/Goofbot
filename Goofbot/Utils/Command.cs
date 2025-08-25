128aec2eaf5c20e0c2940c0165a2cf9cecfd893f
Jack Tackett <jeanvaljean563@gmail.com>
2025-08-24 23:45:23 -0500

    added CommandAccessibilityModifier enum

--- Goofbot/Utils/Command.cs
+++ Goofbot/Utils/Command.cs
@@ -10,15 +10,15 @@ internal class Command
     private readonly object module;
     private readonly Func<string, OnChatCommandReceivedArgs, bool, Task<string>> commandAction;
     private readonly TimeSpan timeout;
-    private readonly bool goofOnly;
+    private readonly CommandAccessibilityModifier commandAccessibilityModifier;
 
     private DateTime timeOfLastInvocation = DateTime.MinValue;
 
-    public Command(string name, Func<string, OnChatCommandReceivedArgs, bool, Task<string>> commandAction, int timeoutSeconds, bool goofOnly = false)
+    public Command(string name, Func<string, OnChatCommandReceivedArgs, bool, Task<string>> commandAction, int timeoutSeconds, CommandAccessibilityModifier commandAccessibilityModifier = CommandAccessibilityModifier.AllChatters)
     {
         this.name = name;
         this.timeout = TimeSpan.FromSeconds(timeoutSeconds);
-        this.goofOnly = goofOnly;
+        this.commandAccessibilityModifier = commandAccessibilityModifier;
         this.commandAction = commandAction;
     }
 
@@ -29,9 +29,18 @@ internal class Command
 
     public async Task<string> ExecuteCommandAsync(string commandArgs, OnChatCommandReceivedArgs eventArgs, bool isReversed)
     {
-        if (this.goofOnly && !eventArgs.Command.ChatMessage.IsBroadcaster)
+        if (this.commandAccessibilityModifier == CommandAccessibilityModifier.StreamerOnly && !eventArgs.Command.ChatMessage.IsBroadcaster)
         {
-            return "I don't answer to you, peasant! OhMyDog (this command is for Goof's use only)";
+            // return "I don't answer to you, peasant! OhMyDog (this command is for Goof's use only)";
+            return string.Empty;
+        }
+        else if (this.commandAccessibilityModifier == CommandAccessibilityModifier.SubOnly && !eventArgs.Command.ChatMessage.IsSubscriber)
+        {
+            return string.Empty;
+        }
+        else if (this.commandAccessibilityModifier == CommandAccessibilityModifier.ModOnly && !eventArgs.Command.ChatMessage.IsModerator)
+        {
+            return string.Empty;
         }
 
         DateTime invocationTime = DateTime.UtcNow;
@@ -46,4 +55,12 @@ internal class Command
             return string.Empty;
         }
     }
+}
+
+public enum CommandAccessibilityModifier
+{
+    StreamerOnly,
+    SubOnly,
+    ModOnly,
+    AllChatters,
 }
