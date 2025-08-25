3461af25f21677acbd3509af4f8a6c17c685da84
Jack Tackett <jeanvaljean563@gmail.com>
2025-08-25 01:36:47 -0500

    Added TTS channel point reward and !cancel

--- Goofbot/Modules/TextToSpeechModule.cs
+++ Goofbot/Modules/TextToSpeechModule.cs
@@ -12,19 +12,25 @@ using System.Threading.Tasks;
 using TwitchLib.Api;
 using TwitchLib.Client;
 using TwitchLib.Client.Events;
+using TwitchLib.EventSub.Websockets.Core.EventArgs.Channel;
 
 [SupportedOSPlatform("windows")]
 internal class TextToSpeechModule : GoofbotModule
 {
-    private readonly SpeechSynthesizer speechSynthesizer = new ();
     private readonly SemaphoreSlim speechSemaphore = new (1, 1);
+
+    private SpeechSynthesizer speechSynthesizer;
 
     public TextToSpeechModule(string moduleDataFolder)
         : base(moduleDataFolder)
     {
+        this.speechSynthesizer = new ();
         this.speechSynthesizer.SetOutputToDefaultAudioDevice();
 
+        Program.EventSubWebsocketClient.ChannelPointsCustomRewardRedemptionAdd += this.OnChannelPointsCustomRewardRedemptionAdd;
+
         Program.CommandDictionary.TryAddCommand(new Command("tts", this.TTSCommand, 1, CommandAccessibilityModifier.SubOnly));
+        Program.CommandDictionary.TryAddCommand(new Command("cancel", this.CancelCommand, 0, CommandAccessibilityModifier.StreamerOnly));
     }
 
     ~TextToSpeechModule()
@@ -38,12 +44,40 @@ internal class TextToSpeechModule : GoofbotModule
         return string.Empty;
     }
 
+    public async Task<string> CancelCommand(string commandArgs, OnChatCommandReceivedArgs eventArgs, bool isReversed)
+    {
+        this.speechSynthesizer.Dispose();
+        this.speechSynthesizer = new ();
+        this.speechSynthesizer.SetOutputToDefaultAudioDevice();
+
+        return string.Empty;
+    }
+
+    private async Task OnChannelPointsCustomRewardRedemptionAdd(object sender, ChannelPointsCustomRewardRedemptionArgs e)
+    {
+        if (e.Notification.Payload.Event.Reward.Title.Equals("TTS"))
+        {
+            string messageToSpeak = e.Notification.Payload.Event.UserInput;
+            await this.SpeakAsync(messageToSpeak);
+        }
+    }
+
     private async Task SpeakAsync(string messageToSpeak)
     {
         await this.speechSemaphore.WaitAsync();
         try
         {
-            await Task.Run(() => this.speechSynthesizer.Speak(messageToSpeak));
+            await Task.Delay(2000);
+            await Task.Run(() =>
+            {
+                try
+                {
+                    this.speechSynthesizer.Speak(messageToSpeak);
+                }
+                catch
+                {
+                }
+            });
         }
         finally
         {
