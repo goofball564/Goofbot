f919b2e331fe566aa1bf10cd30fb135abe8ffbac
Jack Tackett <jeanvaljean563@gmail.com>
2025-09-01 23:50:43 -0500

    untested change to TTS message queue + consumer thread

--- Goofbot/Modules/TextToSpeechModule.cs
+++ Goofbot/Modules/TextToSpeechModule.cs
@@ -1,6 +1,8 @@
 ﻿namespace Goofbot.Modules;
 
 using Goofbot.Utils;
+using System;
+using System.Collections.Concurrent;
 using System.Runtime.Versioning;
 using System.Speech.Synthesis;
 using System.Threading;
@@ -13,24 +15,27 @@ internal class TextToSpeechModule : GoofbotModule
 {
     private const int Volume = 60;
 
-    private readonly SemaphoreSlim speechSemaphore = new (1, 1);
-
-    private SpeechSynthesizer speechSynthesizer;
+    private readonly BlockingCollection<QueuedTTS> ttsQueue = new (new ConcurrentQueue<QueuedTTS>(), 1000);
+    private QueuedTTS currentTTS;
 
     public TextToSpeechModule(string moduleDataFolder)
         : base(moduleDataFolder)
     {
-        this.InitializeSpeechSynthesizer();
+        this.currentTTS = new (string.Empty, string.Empty, this.SpeakSAPI5);
 
         Program.EventSubWebsocketClient.ChannelPointsCustomRewardRedemptionAdd += this.OnChannelPointsCustomRewardRedemptionAdd;
 
         Program.CommandDictionary.TryAddCommand(new Command("tts", this.TTSCommand, 1, CommandAccessibilityModifier.SubOnly));
         Program.CommandDictionary.TryAddCommand(new Command("emergencystop", this.EmergencyStopCommand, 0, CommandAccessibilityModifier.StreamerOnly));
-    }
 
-    ~TextToSpeechModule()
-    {
-        this.speechSynthesizer.Dispose();
+        Task runningTask = Task.Run(async () =>
+        {
+            while (true)
+            {
+                this.currentTTS = this.ttsQueue.Take();
+                await this.currentTTS.Execute();
+            }
+        });
     }
 
     public async Task<string> TTSCommand(string commandArgs, OnChatCommandReceivedArgs eventArgs, bool isReversed)
@@ -41,55 +46,93 @@ internal class TextToSpeechModule : GoofbotModule
         }
         else
         {
-            await this.SpeakAsync(commandArgs);
+            string username = eventArgs.Command.ChatMessage.DisplayName.ToLowerInvariant();
+            this.ttsQueue.Add(new QueuedTTS(username, commandArgs, this.SpeakSAPI5));
             return string.Empty;
         }
     }
 
     public async Task<string> EmergencyStopCommand(string commandArgs, OnChatCommandReceivedArgs eventArgs, bool isReversed)
     {
-        this.speechSynthesizer.Dispose();
-        this.InitializeSpeechSynthesizer();
+        foreach (QueuedTTS tts in this.ttsQueue)
+        {
+            tts.CancellationTokenSource.Cancel();
+        }
+
+        this.currentTTS.CancellationTokenSource.Cancel();
 
         return string.Empty;
     }
 
-    private void InitializeSpeechSynthesizer()
+    private SpeechSynthesizer InitializeSpeechSynthesizer()
     {
-        this.speechSynthesizer = new ();
-        this.speechSynthesizer.SetOutputToDefaultAudioDevice();
-        this.speechSynthesizer.Volume = Volume;
+        SpeechSynthesizer speechSynthesizer = new ();
+        speechSynthesizer.SetOutputToDefaultAudioDevice();
+        speechSynthesizer.Volume = Volume;
+        return speechSynthesizer;
+    }
+
+    private async Task SpeakSAPI5(string message, CancellationToken cancellationToken)
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return;
+        }
+
+        await Task.Delay(2000);
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return;
+        }
+
+        using (SpeechSynthesizer speechSynthesizer = this.InitializeSpeechSynthesizer())
+        {
+            Prompt speechPrompt = speechSynthesizer.SpeakAsync(message);
+            while (!(speechPrompt.IsCompleted || cancellationToken.IsCancellationRequested))
+            {
+                await Task.Delay(500);
+            }
+        }
     }
 
     private async Task OnChannelPointsCustomRewardRedemptionAdd(object sender, ChannelPointsCustomRewardRedemptionArgs e)
     {
         if (e.Notification.Payload.Event.Reward.Title.Equals("TTS"))
         {
-            string messageToSpeak = e.Notification.Payload.Event.UserInput;
-            await this.SpeakAsync(messageToSpeak);
+            string message = e.Notification.Payload.Event.UserInput;
+            string username = e.Notification.Payload.Event.UserName.ToLowerInvariant();
+
+            this.ttsQueue.Add(new QueuedTTS(username, message, this.SpeakSAPI5));
         }
     }
+}
 
-    private async Task SpeakAsync(string messageToSpeak)
+internal class QueuedTTS
+{
+    public QueuedTTS(string username, string message, Func<string, CancellationToken, Task> action)
     {
-        await this.speechSemaphore.WaitAsync();
-        try
-        {
-            await Task.Delay(2000);
-            await Task.Run(() =>
-            {
-                try
-                {
-                    this.speechSynthesizer.Speak(messageToSpeak);
-                }
-                catch
-                {
-                }
-            });
-        }
-        finally
-        {
-            this.speechSemaphore.Release();
-        }
+        this.Username = username;
+        this.Message = message;
+        this.Action = action;
+
+        this.CancellationTokenSource = new ();
+    }
+
+    ~QueuedTTS()
+    {
+        this.CancellationTokenSource.Dispose();
+    }
+
+    public string Username { get; private set; }
+
+    public string Message { get; private set; }
+
+    public CancellationTokenSource CancellationTokenSource { get; private set; }
+
+    public Func<string, CancellationToken, Task> Action { get; private set; }
+
+    public async Task Execute()
+    {
+        await this.Action(this.Message, this.CancellationTokenSource.Token);
     }
 }
