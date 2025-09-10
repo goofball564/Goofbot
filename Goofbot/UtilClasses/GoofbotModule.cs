b16e2032d8608008b06eb24390046b0ab6231c34
Jack Tackett <jeanvaljean563@gmail.com>
2025-09-09 20:57:48 -0500

    All GoofbotModule classes will create a database connection

--- Goofbot/UtilClasses/GoofbotModule.cs
+++ Goofbot/UtilClasses/GoofbotModule.cs
@@ -1,20 +1,30 @@
 ﻿namespace Goofbot.Utils;
 
+using Microsoft.Data.Sqlite;
 using System;
 using System.IO;
 
 internal abstract class GoofbotModule : IDisposable
 {
     protected readonly string moduleDataFolder;
+    protected readonly SqliteConnection sqliteConnection;
+
+    private const string ModuleDatabaseFile = "data.db";
 
     protected GoofbotModule(string moduleDataFolder)
     {
         this.moduleDataFolder = Path.Join(Program.StuffFolder, moduleDataFolder);
+        Directory.CreateDirectory(this.moduleDataFolder);
 
+        SqliteConnectionStringBuilder connectionStringBuilder = [];
+        connectionStringBuilder.DataSource = ModuleDatabaseFile;
+
+        this.sqliteConnection = new SqliteConnection(connectionStringBuilder.ConnectionString);
+        this.sqliteConnection.Open();
     }
 
     public virtual void Dispose()
     {
-
+        this.sqliteConnection.Dispose();
     }
 }
