40ff56a2b0a44d7bfb417a87776857d6c1e8eab8
Jack Tackett <jeanvaljean563@gmail.com>
2025-09-15 20:46:32 -0500

    Commands for creating table and incrementing token count for a user

--- Goofbot/Modules/CheckInTokenModule.cs
+++ Goofbot/Modules/CheckInTokenModule.cs
@@ -2,7 +2,6 @@
 
 using Goofbot.UtilClasses;
 using Microsoft.Data.Sqlite;
-using System;
 using System.Threading.Tasks;
 using TwitchLib.EventSub.Websockets.Core.EventArgs.Channel;
 
@@ -19,15 +18,40 @@ internal class CheckInTokenModule : GoofbotModule
         string reward = e.Notification.Payload.Event.Reward.Title;
         string userID = e.Notification.Payload.Event.UserId;
         string userName = e.Notification.Payload.Event.UserName;
-
-        /*if (reward.Equals("howdy pardner", StringComparison.OrdinalIgnoreCase))
-        {
-            await this.bot.InsertOrUpdateUserAsync(userID, userName);
-        }*/
     }
 
     private async Task CreateTokenCountsTableAsync()
     {
+        using var sqliteConnection = this.bot.OpenSqliteConnection();
+        using var createTableCommand = new SqliteCommand();
+        createTableCommand.CommandText =
+            @"CREATE TABLE IF NOT EXISTS CheckInTokenModule_TokenCounts (
+                UserID INTEGER PRIMARY KEY,
+                TokenCount INTEGER NOT NULL,
+                LastUpdateTimestamp REAL NOT NULL,
+                FOREIGN KEY(UserID) REFERENCES TwitchUsers(UserID)
+            );
 
+            CREATE INDEX IF NOT EXISTS CheckInTokenModule_TokenCountsIdx1 ON TokenCounts (TokenCount DESC, LastUpdateTimestamp ASC);";
+        createTableCommand.Connection = sqliteConnection;
+        using (await this.bot.SqliteReaderWriterLock.WriteLockAsync())
+        {
+            await createTableCommand.ExecuteNonQueryAsync();
+        }
+    }
+
+    private async Task IncrementTokens(string userID)
+    {
+        using var sqliteConnection = this.bot.OpenSqliteConnection();
+        using var updateCommand = new SqliteCommand();
+        updateCommand.CommandText =
+            @"INSERT INTO CheckInTokenModule_TokenCounts VALUES (@UserID, 1, unixepoch('now','subsec')) 
+            ON CONFLICT(UserID) DO UPDATE SET TokenCount = TokenCount + 1;";
+        updateCommand.Parameters.AddWithValue("@UserID", int.Parse(userID));
+        updateCommand.Connection = sqliteConnection;
+        using (await this.bot.SqliteReaderWriterLock.WriteLockAsync())
+        {
+            await updateCommand.ExecuteReaderAsync();
+        }
     }
 }
