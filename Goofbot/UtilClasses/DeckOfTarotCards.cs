8ab0bb39eef8645b433fbc0afbeb4f1b5c9b2462
Jack Tackett <jeanvaljean563@gmail.com>
2025-09-21 08:24:17 -0500

    remove unused usings

--- Goofbot/UtilClasses/DeckOfTarotCards.cs
+++ Goofbot/UtilClasses/DeckOfTarotCards.cs
@@ -2,10 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 internal class DeckOfTarotCards
 {
