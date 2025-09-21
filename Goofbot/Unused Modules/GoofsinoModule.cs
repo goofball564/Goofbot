ce37988696e84ed10f778d9e70095e26b066cd9a
Jack Tackett <jeanvaljean563@gmail.com>
2025-09-21 08:25:11 -0500

    added roulettetable class, sql commands for bets

--- Goofbot/Unused Modules/GoofsinoModule.cs
+++ Goofbot/Unused Modules/GoofsinoModule.cs
@@ -1,18 +1,215 @@
 ﻿namespace Goofbot.Modules;
-// Goofsino
-// Has game(s)
-// Users can join game(s)
 
-// Game
-// Users place bet on outcomes
-// List of bets and can't take back, can add more, can add multiple types
-// Logic happens
-// Bets are paid out at the end
+using Goofbot.UtilClasses;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Threading.Tasks;
 
-// Bet
-// type of bet
-// amount
-// who placed it
-internal class GoofsinoModule
+internal class GoofsinoModule : GoofbotModule
 {
+    private static readonly Bet RouletteColumn1 = new (2, 2);
+    private static readonly Bet RouletteColumn2 = new (3, 2);
+    private static readonly Bet RouletteColumn3 = new (4, 2);
+    private static readonly Bet RouletteDozen1 = new (5, 2);
+    private static readonly Bet RouletteDozen2 = new (6, 2);
+    private static readonly Bet RouletteDozen3 = new (7, 2);
+    private static readonly Bet RouletteHigh = new (8, 1);
+    private static readonly Bet RouletteLow = new (9, 1);
+    private static readonly Bet RouletteEven = new (10, 1);
+    private static readonly Bet RouletteOdd = new (11, 1);
+    private static readonly Bet RouletteRed = new (12, 1);
+    private static readonly Bet RouletteBlack = new (13, 1);
+    private static readonly Bet RouletteTopLine = new (14, 6);
+
+    public GoofsinoModule(Bot bot, string moduleDataFolder)
+        : base(bot, moduleDataFolder)
+    {
+    }
+
+    public async Task InitializeAsync()
+    {
+        using (var sqliteConnection = this.bot.OpenSqliteConnection())
+        using (await this.bot.SqliteReaderWriterLock.WriteLockAsync())
+        {
+            await CreateTablesAsync(sqliteConnection);
+        }
+    }
+
+    private static async Task<bool> CreateTablesAsync(SqliteConnection sqliteConnection)
+    {
+        using var transaction = sqliteConnection.BeginTransaction();
+        try
+        {
+            using var sqliteCommand = new SqliteCommand(null, sqliteConnection);
+            sqliteCommand.CommandText =
+                @"CREATE TABLE IF NOT EXISTS Bets (
+                UserID INTEGER NOT NULL,
+                BetTypeID INTEGER NOT NULL,
+                Amount INTEGER NOT NULL,
+                PRIMARY KEY (UserID, BetTypeID),
+                FOREIGN KEY(UserID) REFERENCES TwitchUsers(UserID)
+            ) WITHOUT ROWID;
+
+            CREATE INDEX IF NOT EXISTS BetsIDsIdx ON Bets (BetTypeID);
+
+            CREATE TABLE IF NOT EXISTS GambaPoints (
+                UserID INTEGER PRIMARY KEY,
+                Balance INTEGER NOT NULL,
+                LastUpdateTimestamp REAL NOT NULL,
+                FOREIGN KEY(UserID) REFERENCES TwitchUsers(UserID)
+            );
+
+            CREATE INDEX IF NOT EXISTS GambaPointsBalanceIdx ON GambaPoints (Balance DESC, LastUpdateTimestamp ASC);
+
+            CREATE TABLE IF NOT EXISTS Bankruptcies (
+                UserID INTEGER PRIMARY KEY,
+                Count INTEGER NOT NULL,
+                LastUpdateTimestamp REAL NOT NULL,
+                FOREIGN KEY(UserID) REFERENCES TwitchUsers(UserID)
+            );
+
+            CREATE INDEX IF NOT EXISTS BankruptcyCountsIdx ON Bankruptcies (Count DESC, LastUpdateTimestamp ASC);
+        ";
+
+            await sqliteCommand.ExecuteNonQueryAsync();
+            await transaction.CommitAsync();
+            return true;
+        }
+        catch (SqliteException)
+        {
+            await transaction.RollbackAsync();
+            return false;
+        }
+    }
+
+    private static async Task AddUserToGambaPointsTableAsync(SqliteConnection sqliteConnection, string userID)
+    {
+        using var sqliteCommand = new SqliteCommand(null, sqliteConnection);
+        sqliteCommand.CommandText = "INSERT INTO GambaPoints VALUES (@UserID, 1000, unixepoch('now','subsec')) ON CONFLICT DO NOTHING;";
+        sqliteCommand.Parameters.AddWithValue("@UserID", long.Parse(userID));
+
+        await sqliteCommand.ExecuteNonQueryAsync();
+    }
+
+    private static async Task<bool> ResetUserGambaPointsBalanceAndIncrementBankruptciesAsync(SqliteConnection sqliteConnection, string userID)
+    {
+        using var transaction = sqliteConnection.BeginTransaction();
+        try
+        {
+            using var replaceCommand = new SqliteCommand(null, sqliteConnection);
+            replaceCommand.CommandText = "REPLACE INTO GambaPoints VALUES (@UserID, 1000, unixepoch('now','subsec'));";
+            replaceCommand.Parameters.AddWithValue("@UserID", long.Parse(userID));
+
+            using var updateCommand = new SqliteCommand(null, sqliteConnection);
+            updateCommand.CommandText =
+                @"INSERT INTO Bankruptcies VALUES (@UserID, 1, unixepoch('now','subsec'))
+                            ON CONFLICT(UserID) DO UPDATE SET Count = Count + 1;";
+            updateCommand.Parameters.AddWithValue("@UserID", long.Parse(userID));
+
+            await replaceCommand.ExecuteNonQueryAsync();
+            await updateCommand.ExecuteNonQueryAsync();
+            await transaction.CommitAsync();
+            return true;
+        }
+        catch (SqliteException)
+        {
+            await transaction.RollbackAsync();
+            return false;
+        }
+    }
+
+    private static async Task<bool> ResolveBetAsync(SqliteConnection sqliteConnection, string userID, Bet bet, bool success)
+    {
+        using var transaction = sqliteConnection.BeginTransaction();
+        try
+        {
+            await transaction.CommitAsync();
+            return true;
+        }
+        catch (SqliteException)
+        {
+            await transaction.RollbackAsync();
+            return false;
+        }
+    }
+
+    private static async Task<long> GetBalanceAsync(SqliteConnection sqliteConnection, string userID)
+    {
+        using var sqliteCommand = new SqliteCommand(null, sqliteConnection);
+        sqliteCommand.CommandText = "SELECT Balance FROM GambaPoints WHERE UserID = @UserID;";
+        sqliteCommand.Parameters.AddWithValue("@UserID", long.Parse(userID));
+
+        return Convert.ToInt64(await sqliteCommand.ExecuteScalarAsync());
+    }
+
+    private static async Task<long> GetTotalBetsAsync(SqliteConnection sqliteConnection, string userID)
+    {
+        using var sqliteCommand = new SqliteCommand(null, sqliteConnection);
+        sqliteCommand.CommandText = "SELECT SUM(Amount) FROM Bets WHERE UserID = @UserID GROUP BY UserID;";
+        sqliteCommand.Parameters.AddWithValue("@UserID", long.Parse(userID));
+
+        return Convert.ToInt64(await sqliteCommand.ExecuteScalarAsync());
+    }
+
+    private static async Task<long> GetBetAmountAsync(SqliteConnection sqliteConnection, string userID, Bet bet)
+    {
+        using var sqliteCommand = new SqliteCommand(null, sqliteConnection);
+        sqliteCommand.CommandText = "SELECT Amount FROM Bets WHERE UserID = @UserID AND BetTypeID = @BetTypeID;";
+        sqliteCommand.Parameters.AddWithValue("@UserID", long.Parse(userID));
+        sqliteCommand.Parameters.AddWithValue("@BetTypeID", bet.TypeID);
+
+        return Convert.ToInt64(await sqliteCommand.ExecuteScalarAsync());
+    }
+
+    private static async Task AddBetToBetsTableAsync(SqliteConnection sqliteConnection, string userID, Bet bet, long amount)
+    {
+        using var sqliteCommand = new SqliteCommand(null, sqliteConnection);
+        sqliteCommand.CommandText = "INSERT INTO Bets VALUES (@UserID, @BetTypeID, @Amount) ON CONFLICT DO UPDATE SET Amount = Amount + @Amount;";
+        sqliteCommand.Parameters.AddWithValue("@UserID", long.Parse(userID));
+        sqliteCommand.Parameters.AddWithValue("@BetTypeID", bet.TypeID);
+        sqliteCommand.Parameters.AddWithValue("@Amount", amount);
+
+        await sqliteCommand.ExecuteNonQueryAsync();
+    }
+
+    private static async Task AddBalanceAsync(SqliteConnection sqliteConnection, string userID, long amount)
+    {
+        using var sqliteCommand = new SqliteCommand(null, sqliteConnection);
+        sqliteCommand.CommandText = "UPDATE GambaPoints SET Balance = Balance + @Amount WHERE UserID = @UserID;";
+        sqliteCommand.Parameters.AddWithValue("@UserID", long.Parse(userID));
+        sqliteCommand.Parameters.AddWithValue("@Amount", amount);
+
+        await sqliteCommand.ExecuteNonQueryAsync();
+    }
+
+    private async Task<bool> TryPlaceBetAsync(string userID, Bet bet, long amount)
+    {
+        using (await this.bot.SqliteReaderWriterLock.WriteLockAsync())
+        using (var sqliteConnection = new SqliteConnection())
+        {
+            long balance = await GetBalanceAsync(sqliteConnection, userID);
+            long totalBets = await GetTotalBetsAsync(sqliteConnection, userID);
+
+            if (amount + totalBets <= balance)
+            {
+                await AddBetToBetsTableAsync(sqliteConnection, userID, bet, amount);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    private async Task<bool> TryDeclareBankruptcy(string userID)
+    {
+        return true;
+    }
+
+    private readonly struct Bet(long typeID, long payoutRatio)
+    {
+        public readonly long TypeID = typeID;
+        public readonly long PayoutRatio = payoutRatio;
+    }
 }
---
+++ Goofbot/UtilClasses/RouletteTable.cs
@@ -0,0 +1,128 @@
+﻿namespace Goofbot.UtilClasses;
+
+using System;
+using System.Security.Cryptography;
+
+internal class RouletteTable
+{
+    private int lastSpinResult = 0;
+
+    public enum RouletteColor
+    {
+        Red,
+        Black,
+        Green,
+    }
+
+    public string LastSpinResult
+    {
+        get
+        {
+            if (this.lastSpinResult < 0)
+            {
+                return "00";
+            }
+            else
+            {
+                return this.lastSpinResult.ToString();
+            }
+        }
+    }
+
+    public RouletteColor Color
+    {
+        get
+        {
+            if ((this.lastSpinResult >= 1 && this.lastSpinResult <= 10) || (this.lastSpinResult >= 19 && this.lastSpinResult <= 28))
+            {
+                return this.lastSpinResult % 2 == 0 ? RouletteColor.Black : RouletteColor.Red;
+            }
+            else if ((this.lastSpinResult >= 11 && this.lastSpinResult <= 18) || (this.lastSpinResult >= 29 && this.lastSpinResult <= 36))
+            {
+                return this.lastSpinResult % 2 == 0 ? RouletteColor.Red : RouletteColor.Black;
+            }
+            else
+            {
+                return RouletteColor.Green;
+            }
+        }
+    }
+
+    // Returns 0 if last spin was 00 or 0
+    public int Column
+    {
+        get
+        {
+            if (this.lastSpinResult <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int column = this.lastSpinResult % 3;
+                return column > 0 ? column : 3;
+            }
+        }
+    }
+
+    public int Dozen
+    {
+        get
+        {
+            return Math.Max(Convert.ToInt32(Math.Ceiling(this.lastSpinResult / 12.0)), 0);
+        }
+    }
+
+    public bool High
+    {
+        get
+        {
+            return this.lastSpinResult >= 19;
+        }
+    }
+
+    public bool Low
+    {
+        get
+        {
+            return this.lastSpinResult <= 18 && this.lastSpinResult >= 1;
+        }
+    }
+
+    public bool Even
+    {
+        get
+        {
+            return this.lastSpinResult > 0 && this.lastSpinResult % 2 == 0;
+        }
+    }
+
+    public bool Odd
+    {
+        get
+        {
+            return this.lastSpinResult > 0 && this.lastSpinResult % 2 == 1;
+        }
+    }
+
+    public bool TopLine
+    {
+        get
+        {
+            return this.lastSpinResult <= 3;
+        }
+    }
+
+    public int Street
+    {
+        get
+        {
+            return Math.Max(Convert.ToInt32(Math.Ceiling(this.lastSpinResult / 3.0)), 0);
+        }
+    }
+
+    public void Spin()
+    {
+        this.lastSpinResult = RandomNumberGenerator.GetInt32(38) - 1;
+    }
+}
