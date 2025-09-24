182f64f06439988d494fa15094f492e89f7fce45
goofball564 <jeanvaljean563@gmail.com>
2025-09-24 05:30:39 -0500

    remove unused usings

--- Goofbot/Structs/UserNameAndCount.cs
+++ Goofbot/Structs/UserNameAndCount.cs
@@ -1,12 +1,4 @@
 ﻿namespace Goofbot.Structs;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
 internal readonly struct UserNameAndCount(string userName, long count)
 {
     public readonly string UserName = userName;
