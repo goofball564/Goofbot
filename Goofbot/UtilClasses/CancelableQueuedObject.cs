1864e63d12d3632e2c818f6cb3fcf0c20c34e954
Jack Tackett <jeanvaljean563@gmail.com>
2025-10-08 20:08:15 -0500

    Add type

---
+++ Goofbot/UtilClasses/CancelableQueuedObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Goofbot.UtilClasses;
+
+internal class CancelableQueuedObject<T>(T value)
+{
+    public readonly T Value = value;
+
+    public bool Canceled { get; private set; } = false;
+
+    public void Cancel()
+    {
+        this.Canceled = true;
+    }
+}
